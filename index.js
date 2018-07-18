80f0ece64e49d211cf2e33ec813c8c8ef4161bea
Christine Patton <christine@pattoncreative.com>
2018-07-18 23:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 /* Enter the code to remove the main node element under this comment */
+mainElement=document.body.querySelector('main')
+main.remove()
+document.body.querySelector('main').remove()
+
 
 document.body.querySelector('main').remove()
 
 
 /* Create your new element here and assign it to newHeader */
+<<<<<<< HEAD
 
 const newHeader=document.createElement('h1')
 document.body.appendChild(newHeader)
 newHeader.id='victory'
 newHeader.innerText = 'Christine is the champion'
+=======
+const newHeader = h1#victory;
+var element=document.createlement(h1)
+document.body.appendChild(h1)
+>>>>>>> 115582c782ec4b8fb25b37f81395661d3564f34f
 
 
